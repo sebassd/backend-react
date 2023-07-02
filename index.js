32015fad2d44d11e52f78721e31eefcf9e9d9ca9
Sebastian Simancas Gonzalez <37217266+sebassd@users.noreply.github.com>
2023-07-02 19:57:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
+
 
 const app = express();
 const PORT = 3009;
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet());
+
 // Define your routes here
 
 const RoomsRouter = require('./routers/RoomsRouter');
